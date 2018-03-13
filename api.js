cafed144cda035a4f998e2721bd6042be090801c
Ganesh babu <ganeshbabu.m@inxsasia.com>
2018-03-14 00:36:33 +0530

    added express + cors

--- api.js
+++ api.js
@@ -1,5 +1,17 @@
-const io = require('socket.io')();
+// const io = require('socket.io')();
+var app = require('express')();
+var cors = require('cors');
 
+//app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+  }
+);
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 io.on('connection', (client) => {
     client.on('subscribeToTimer', (interval) => {
       console.log('client is subscribing to timer with interval ', interval);
